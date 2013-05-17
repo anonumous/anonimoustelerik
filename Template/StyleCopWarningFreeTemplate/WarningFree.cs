1c6f130df7f76c14d1ab2b9d336be1c4f96f3b9d
rosteslav <rosteslav.yordanov@gmail.com>
2013-05-17 23:13:22 +0300

    StyleCopNoWarningMessages

---
+++ Template/StyleCopWarningFreeTemplate/WarningFree.cs
@@ -0,0 +1,23 @@
+﻿[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "Reviewed. Suppression is OK here.")]
+
+namespace StyleCopWarningFreeTemplate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    
+    /// <summary>
+    /// Some documentation on the class
+    /// </summary>
+    public class WarningFree
+    {
+        /// <summary>
+        /// Some documentation on the Main method
+        /// </summary>
+        public static void Main()
+        {
+        }
+    }
+}
